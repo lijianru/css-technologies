56cd5a8edb8fdbb4774237bc52667986c5c74f9f
lijianru <2219269374@qq.com>
2022-10-25 15:51:19 +0800

    styled-components基础使用

--- css-in-js/styled-components/src/App.tsx
+++ css-in-js/styled-components/src/App.tsx
@@ -1,27 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
+import { Select } from 'antd';
+import styled, { keyframes, ThemeProvider } from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const StyledSelect = styled(Select)`
+  display: block;
+  width: ${(props) => (props.defaultValue === 'jack' ? '200px' : '400px')};
+`;
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: ${(props) => props.color || 'red'};
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+const StyledDiv = styled.div`
+  color: ${(props) => props.theme.main};
+
+  &:hover {
+    color: red;
+  }
+`;
+
+const StyledInput = styled.input.attrs((props) => ({
+  type: 'text',
+  size: props.size || '1em',
+}))`
+  margin: ${(props) => props.size};
+  padding: ${(props) => props.size};
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Rotate = styled.div`
+  animation: ${rotate} 2s linear infinite;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button type="primary">click</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider
+      theme={{
+        main: 'blue',
+      }}
+    >
+      <div className="App">
+        <Wrapper>
+          <Title>Hello World!</Title>
+        </Wrapper>
+        <StyledSelect defaultValue="jack">
+          <Select.Option value="jack">Jack</Select.Option>
+        </StyledSelect>
+        <StyledSelect defaultValue="jack1">
+          <Select.Option value="jack1">Jack1</Select.Option>
+        </StyledSelect>
+        <Input defaultValue="@probablyup" type="text" />
+        <Input defaultValue="@geelen" type="text" color="blue" />
+        <StyledDiv>Hello world!</StyledDiv>
+        <StyledInput placeholder="A small text input" />
+        <StyledInput placeholder="A bigger text input" size="2em" />
+        <Rotate>&lt; 💅🏾 &gt;</Rotate>
+      </div>
+    </ThemeProvider>
   );
 }
 
