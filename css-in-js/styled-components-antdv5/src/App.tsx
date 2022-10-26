9197974a9a9f242b6ec75b77a2eeb6fe2e1cdc91
lijianru <2219269374@qq.com>
2022-10-26 11:37:18 +0800

    antdv5

--- css-in-js/styled-components-antdv5/src/App.tsx
+++ css-in-js/styled-components-antdv5/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import styled from 'styled-components';
+
+const StyledButton = styled(Button)`
+  margin: 20px;
+  background-color: red;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledButton>Click</StyledButton>
     </div>
   );
 }
