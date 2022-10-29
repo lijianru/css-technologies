cbe9d30e6f9c07840e53a810f8328b9ff80d20a6
lijianru <2219269374@qq.com>
2022-10-29 16:51:15 +0800

    dynamic styles

--- css-in-js/emotion/src/EmotionReact.tsx
+++ css-in-js/emotion/src/EmotionReact.tsx
@@ -1,12 +1,15 @@
 // The next line is required for the css prop to work!
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { Button } from 'antd';
+
+const color = 'black';
 
 const style = css`
   color: hotpink;
   &:hover {
-    background-color: black;
+    background-color: ${color};
   }
 `;
 
@@ -16,17 +19,38 @@ const anotherStyle = css({
   color: 'hotpink',
   textDecoration: 'underline',
   '&:hover': {
-    backgroundColor: 'black',
+    backgroundColor: color,
   },
 });
 
 const AnotherComponent = () => <div css={anotherStyle}>Some text with an underline.</div>;
+
+const inputStringStyle = (props: any) => css`
+  background-color: ${color};
+  color: ${props.color};
+`;
+
+const StyledInput1 = styled.input`
+  ${inputStringStyle}
+`;
+
+const inputObjectStyle = (props: any) =>
+  css({
+    backgroundColor: color,
+    color: props.color,
+  });
+
+const StyledInput2 = styled.input`
+  ${inputObjectStyle}
+`;
 
 export function EmotionReact() {
   return (
     <>
       <SomeComponent />
       <AnotherComponent />
+      <StyledInput1 color="green" />
+      <StyledInput2 color="red" />
       <Button css={style}>EmotionReact</Button>
     </>
   );
--- css-in-js/emotion/src/EmotionStyled.tsx
+++ css-in-js/emotion/src/EmotionStyled.tsx
@@ -1,22 +1,27 @@
 import styled from '@emotion/styled';
 import { Button } from 'antd';
 
+const color = 'red';
+
 const StyledButton = styled.button`
-  background-color: red;
-  margin: 10px;
-  padding: 10px;
+  background-color: ${color};
+  color: ${(props) => (props.disabled ? 'black' : 'white')};
 `;
 
 const StyledAntdButton = styled(Button)`
-  background-color: red;
+  background-color: ${color};
   margin: 10px;
+  color: ${(props) => (props.type === 'primary' ? 'black' : 'white')};
+  &:hover {
+    background-color: white;
+  }
 `;
 
 export function EmotionStyled() {
   return (
     <>
-      <StyledButton>Click</StyledButton>
-      <StyledAntdButton>Click StyledAntdButton</StyledAntdButton>
+      <StyledButton disabled>Click</StyledButton>
+      <StyledAntdButton type="primary">Click StyledAntdButton</StyledAntdButton>
     </>
   );
 }
